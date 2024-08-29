935efec6e4a49f79474a3afd39bc0c555fc54594
chohangmin <slightsmille@gmail.com>
2024-08-29 15:40:41 +0900

    ?

--- lib/screen/group_chat_list.dart
+++ lib/screen/group_chat_list.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class GroupChatList extends StatelessWidget {
@@ -12,7 +14,38 @@ class GroupChatList extends StatelessWidget {
             bottom: 100,
             left: 10,
             child: FloatingActionButton.extended(
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => Dialog(
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            width: 200,
+                            height: 400,
+                            child: StreamBuilder(
+                              stream: FirebaseFirestore.instance
+                                  .collection('usrs')
+                                  .orderBy('isConnecting', descending: true)
+                                  .snapshots(),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<
+                                          QuerySnapshot<Map<String, dynamic>>>
+                                      snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
+
+                                final userDocs = snapshot.data!.docs;
+
+                                
+                              },
+                            ),
+                          ),
+                        ));
+              },
               label: const Text('Add Group'),
               icon: const Icon(Icons.person_add),
             ),
